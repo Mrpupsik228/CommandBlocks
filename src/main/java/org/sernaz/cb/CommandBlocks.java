0ed3469b12a277bba1f8542b36b25622533d095f
Mrpupsik228 <cubertank567u@gmail.com>
2025-11-23 16:04:16 +0100

    Initial commit

---
+++ src/main/java/org/sernaz/cb/CommandBlocks.java
@@ -0,0 +1,10 @@
+package org.sernaz.cb;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CommandBlocks implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/org/sernaz/cb/client/CommandBlocksClient.java
@@ -0,0 +1,10 @@
+package org.sernaz.cb.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CommandBlocksClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
